40577509854324973ae54ed9775062d38280b41a
im4everok <imizyk09@gmail.com>
2023-08-22 20:08:50 +0300

    added items controller and get by id and post methods

---
+++ src/Play.Inventory.Service/Controllers/ItemsController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using Play.Common;
+using Play.Inventory.Service.Dtos;
+using Play.Inventory.Service.Entities;
+
+namespace Play.Inventory.Service.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IRepository<InventoryItem> itemsRepository;
+
+        public ItemsController(IRepository<InventoryItem> itemsRepository)
+        {
+            this.itemsRepository = itemsRepository;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync(Guid userId)
+        {
+            if (userId == Guid.Empty)
+            {
+                return BadRequest();
+            }
+
+            var items = (await itemsRepository.GetAllAsync(i => i.UserId == userId))
+                .Select(i => i.AsDto());
+
+            return Ok(items);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<InventoryItemDto>> PostAsync(GrantItemsDto grantItemsDto)
+        {
+            var inventoryItem = await itemsRepository.GetAsync(i => i.UserId == grantItemsDto.UserId
+            && i.CatalogItemId == grantItemsDto.CatalogItemId);
+
+            if (inventoryItem == null)
+            {
+                inventoryItem = new InventoryItem
+                {
+                    CatalogItemId = grantItemsDto.CatalogItemId,
+                    UserId = grantItemsDto.UserId,
+                    Quantity = grantItemsDto.Quantity,
+                    AcquiredDate = DateTimeOffset.UtcNow
+                };
+
+                await itemsRepository.CreateAsync(inventoryItem);
+            }
+            else
+            {
+                inventoryItem.Quantity += grantItemsDto.Quantity;
+
+                await itemsRepository.UpdateAsync(inventoryItem);
+            }
+
+            return Ok();
+        }
+    }
+}
