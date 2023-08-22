064aabaa8a5a3c8c25340f9a00ba7f51e193387c
im4everok <imizyk09@gmail.com>
2023-08-22 23:29:04 +0300

    added name and description to catalog items

--- src/Play.Inventory.Service/Extensions.cs
+++ src/Play.Inventory.Service/Extensions.cs
@@ -5,9 +5,11 @@ namespace Play.Inventory.Service
 {
     public static class Extensions
     {
-        public static InventoryItemDto AsDto(this InventoryItem item)
+        public static InventoryItemDto AsDto(this InventoryItem item, string Name,
+        string Description)
         {
-            return new InventoryItemDto(item.CatalogItemId, item.Quantity, item.AcquiredDate);
+            return new InventoryItemDto(item.CatalogItemId,
+            Name, Description, item.Quantity, item.AcquiredDate);
         }
     }
 }
