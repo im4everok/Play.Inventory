bbea58a8c219939ded33f2dc6914b31d8bbfd575
im4everok <imizyk09@gmail.com>
2023-08-23 21:07:43 +0300

    added polly and added transient error policy with retry

--- src/Play.Inventory.Service/Program.cs
+++ src/Play.Inventory.Service/Program.cs
@@ -1,6 +1,8 @@
 using Play.Common.MongoDb;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,9 +16,27 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddMongo()
     .AddMongoRepository<InventoryItem>("inventoryitems");
 
-builder.Services.AddHttpClient<CatalogClient>(client => {
+Random jitterer = new();
+
+builder.Services.AddHttpClient<CatalogClient>(client =>
+{
     client.BaseAddress = new Uri("https://localhost:5001");
-});
+})
+.AddTransientHttpErrorPolicy(policyBuilder => policyBuilder
+    .Or<TimeoutRejectedException>()
+    .WaitAndRetryAsync(
+        5,
+        retryAttemptCount => TimeSpan.FromSeconds(Math.Pow(2, retryAttemptCount))
+                            + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+        onRetry: (outcome, timespan, retryAttempt) =>
+        {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            serviceProvider.GetService<ILogger<CatalogClient>>()
+                ?.LogWarning($"Delaying for {timespan.TotalSeconds} secs, then retry #{retryAttempt}");
+        }
+    )
+)
+.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 var app = builder.Build();
 
